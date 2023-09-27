5d06f56208a9eee9dd792a2899363193821165a7
LostoVayne <moises.barrera.dev@gmail.com>
2023-09-27 16:38:39 -0300

    agregadas nuevas pruebas en Navbar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,55 +1,57 @@
-import { useContext } from "react";
-import { Link, NavLink, useNavigate } from "react-router-dom";
-import { AuthContext } from "../../auth";
+import {useContext} from 'react';
+import {Link, NavLink, useNavigate} from 'react-router-dom';
+import {AuthContext} from '../../auth';
 
 export const Navbar = () => {
     const navigate = useNavigate();
 
-    const { user, logged, logout } = useContext(AuthContext);
+    const {user, logged, logout} = useContext(AuthContext);
 
     const onLogout = () => {
         logout();
-
-        navigate("/login", {
+        navigate('/login', {
             replace: true,
         });
     };
 
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
-            <Link className="navbar-brand" to="/">
+        <nav className='navbar navbar-expand-sm navbar-dark bg-dark p-2'>
+            <Link className='navbar-brand' to='/'>
                 Asociaciones
             </Link>
 
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
+            <div className='navbar-collapse'>
+                <div className='navbar-nav'>
                     <NavLink
-                        className={({ isActive }) => (isActive ? "nav-item nav-link active" : "nav-item nav-link")}
-                        to="/marvel"
-                    >
+                        className={({isActive}) =>
+                            isActive ? 'nav-item nav-link active' : 'nav-item nav-link'
+                        }
+                        to='/marvel'>
                         Marvel
                     </NavLink>
 
                     <NavLink
-                        className={({ isActive }) => (isActive ? "nav-item nav-link active" : "nav-item nav-link")}
-                        to="/dc"
-                    >
+                        className={({isActive}) =>
+                            isActive ? 'nav-item nav-link active' : 'nav-item nav-link'
+                        }
+                        to='/dc'>
                         DC
                     </NavLink>
 
                     <NavLink
-                        className={({ isActive }) => (isActive ? "nav-item nav-link active" : "nav-item nav-link")}
-                        to="/search"
-                    >
+                        className={({isActive}) =>
+                            isActive ? 'nav-item nav-link active' : 'nav-item nav-link'
+                        }
+                        to='/search'>
                         Search
                     </NavLink>
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-                <ul className="navbar-nav ml-auto d-flex align-items-center ">
-                    {logged && <span className="nav-item nav-link text-primary ">{user.name}</span>}
-                    <button className="nav-item nav-link btn" onClick={onLogout}>
+            <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
+                <ul className='navbar-nav ml-auto d-flex align-items-center '>
+                    {logged && <span className='nav-item nav-link text-primary '>{user.name}</span>}
+                    <button className='nav-item nav-link btn' onClick={onLogout}>
                         Logout
                     </button>
                 </ul>
--- tests/ui/Navbar.test.jsx
+++ tests/ui/Navbar.test.jsx
@@ -1,3 +1,60 @@
+import {render, screen, fireEvent} from '@testing-library/react';
+import {Navbar} from '../../src/ui';
+import {AuthContext} from '../../src/auth';
+import {MemoryRouter} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+
+const mockedNavigate = jest.fn();
+
+//mock de react router dom
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedNavigate,
+}));
+
 describe('Pruebas en <Navbar />', () => {
-    test('Debe de mostrarse correctamente', () => {});
+    const UserValue = {
+        logged: true,
+        user: {
+            id: 'ABC123',
+            name: 'Camila Doe',
+        },
+        logout: jest.fn(),
+    };
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('Debe de mostrarse el nombre del usuario', () => {
+        render(
+            <AuthContext.Provider value={UserValue}>
+                <MemoryRouter initialEntries={['/marvel']}>
+                    <Navbar />
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        expect(screen.getByText(UserValue.user.name)).toBeTruthy();
+        expect(screen.getByText('Logout')).toBeTruthy();
+    });
+
+    test('Debe de mostrar el boton de logout', () => {
+        render(
+            <AuthContext.Provider value={UserValue}>
+                <MemoryRouter initialEntries={['/marvel']}>
+                    <Navbar />
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        const ButtonLogout = screen.getByRole('button', {name: 'Logout'});
+        expect(ButtonLogout).toBeTruthy();
+        fireEvent.click(ButtonLogout);
+        expect(UserValue.logout).toHaveBeenCalled();
+        //llamada del navigate
+        expect(mockedNavigate).toHaveBeenCalledWith('/login', {
+            replace: true,
+        });
+    });
 });
